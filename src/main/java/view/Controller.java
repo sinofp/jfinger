8c52098bfc9361d3b2ac2b81dc7baf28b27141d4
sinofp <achinese@live.cn>
2018-12-11 23:31:23 +0800

    为javafx组件加上函数
    现在可以在gui中正常压缩了。

--- src/main/java/view/Controller.java
+++ src/main/java/view/Controller.java
@@ -1,28 +1,53 @@
 package view;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import org.zeroturnaround.zip.ZipUtil;
 
 import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class Controller {
+public class Controller implements Initializable {
 
     FileChooser fileChooser = new FileChooser();
+    DirectoryChooser directoryChooser = new DirectoryChooser();
 
     @FXML
     Button btn_zf;
+
     @FXML
     Label mLabel;
 
-    public void chooseFile() {
-        File file = fileChooser.showOpenDialog(new Stage());
-        if(file != null){
-            mLabel.setText(file.getAbsolutePath());
+    public void zipFiles() {
+        File inDir = directoryChooser.showDialog(new Stage());
+        if(inDir!= null){
+            mLabel.setText(inDir.getAbsolutePath());
+            File outDir = directoryChooser.showDialog(new Stage());
+            if (outDir != null) {
+                mLabel.setText(outDir.getAbsolutePath());
+                System.out.println(inDir.getAbsolutePath());
+                System.out.println(inDir.getName());
+                System.out.println(outDir.getAbsolutePath());
+                System.out.println(outDir.getAbsolutePath() + inDir.getName()+ ".zip");
+                ZipUtil.pack(new File(inDir.getAbsolutePath()), new File(outDir.getAbsolutePath() + "/" + inDir.getName()+ ".zip"));
+                mLabel.setText("压缩到了"+outDir.getAbsolutePath());
+            } else {
+                mLabel.setText("没有选择要压缩到的文件夹");
+            }
         }else {
-            mLabel.setText("没有打开任何文件");
+            mLabel.setText("没有选择要压缩的文件夹");
         }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        btn_zf.setText("ZIP");
+        mLabel.setText("点左边的按钮选择要压缩的文件夹、要压缩到的文件夹");
     }
 }
--- src/main/java/view/Main.java
+++ src/main/java/view/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
 
         Parent root = FXMLLoader.load(getClass().getResource("plane.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 600, 50));
         primaryStage.show();
     }
 
