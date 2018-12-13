d5fd5a45e9382f1499364e6221469b2cbf7cbeac
sinofp <achinese@live.cn>
2018-12-13 14:24:11 +0800

    intellij commit first try.

---
+++ src/main/java/top/emptystack/jfinger/view/Main.java
@@ -0,0 +1,25 @@
+package top.emptystack.jfinger.view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        Parent root = FXMLLoader.load(getClass().getResource("scratch.fxml"));
+        primaryStage.setTitle("Jfinger");
+        primaryStage.setScene(new Scene(root, 600, 150));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
